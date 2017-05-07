c53c092c558073af7da2e6c853b3c36893451417
rhdzmota <rhdzmota@mxquants.com>
2017-05-06 22:00:17 -0500

    change exec to eval

--- pyBot.py
+++ pyBot.py
@@ -214,7 +214,7 @@ def optimHandler(text):
     
     # create required variables
     exec(', '.join(variables)+""" = sy.symbols('"""+' '.join(variables)+"""')""")
-    exec('_vars = ['+', '.join(variables)+']')
+    _vars = eval('_vars = ['+', '.join(variables)+']')
     
     # create function
     f = eval(text_func)
